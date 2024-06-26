f548903a5fd6546074c8645bf04667f245a367f2
elyarsa <48180775+elyarsa@users.noreply.github.com>
2024-06-26 13:45:53 +0330

    setup.sh

--- else/setup.sh
+++ else/setup.sh
@@ -6,7 +6,7 @@ if [ $? == 0 ]; then
   echo 'Successfully installed'
 else
   echo 'An error occurred while installing the prerequisites'
-  exit
+  exit 1
 fi
 
 echo 'start redis service'
@@ -15,39 +15,43 @@ if [ $? == 0 ]; then
   echo 'redis service started'
 else
   echo 'service starting failed'
-  exit
+  exit 1
 fi
 
 echo "Creating namizun directory (step 2)"
 mkdir -p /var/www/namizun && cd /var/www/namizun
 
 echo 'Pulling the repository (step 3)'
+if [ -d .git ]; then
+  git remote remove origin
+fi
 git init
 git remote add origin https://github.com/elyarsa/namizun-master-updated.git
 git pull origin main
 if [ $? != 0 ]; then
   echo 'could not clone the repository'
-  exit
+  exit 1
 fi
 
 echo 'Create virtual env (step 4)'
 python3 -m venv /var/www/namizun/venv
 if [ $? != 0 ]; then
   echo "VENV didn't created"
+  exit 1
 fi
 
 echo 'Installing project dependencies (step 5)'
 cd /var/www/namizun && source /var/www/namizun/venv/bin/activate && pip install wheel && pip install namizun_core/ namizun_menu/ && deactivate
 if [ $? != 0 ]; then
   echo "Dependencies doesn't installed correctly"
-  exit
+  exit 1
 fi
 
 echo 'Create namizun service (step 6)'
 ln -s /var/www/namizun/else/namizun.service /etc/systemd/system/
 if [ $? != 0 ]; then
   echo 'Creating service was failed'
-  exit
+  exit 1
 fi
 
 echo 'Reload services and start namizun.service (step 7)'
@@ -56,12 +60,12 @@ sudo systemctl enable namizun.service
 sudo systemctl start namizun.service
 if [ $? != 0 ]; then
   echo "Namizun service didn't started"
-  exit
+  exit 1
 fi
 
 echo "make namizun as a command (step 8)"
 ln -s /var/www/namizun/else/namizun /usr/local/bin/ && chmod +x /usr/local/bin/namizun
 if [ $? != 0 ]; then
   echo "failed to add namizun to PATH environment variables"
-  exit
+  exit 1
 fi
